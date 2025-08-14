aef4bd8b508867b843cc14099d802fc112da174d
gibber9809 <devinbook1@gmail.com>
2025-08-14 16:11:32 +0000

    Modify clp to parse one message per line, even without timestamps

--- components/core/src/clp/MessageParser.cpp
+++ components/core/src/clp/MessageParser.cpp
@@ -93,74 +93,19 @@ bool MessageParser::parse_next_message(
  *   - ...the buffered message is not empty, add the line to the message and continue reading.
  */
 bool MessageParser::parse_line(ParsedMessage& message) {
-    bool message_completed = false;
-
     // Parse timestamp and content
-    TimestampPattern const* timestamp_pattern = message.get_ts_patt();
+    TimestampPattern const* timestamp_pattern = nullptr;
     epochtime_t timestamp = 0;
-    size_t timestamp_begin_pos;
-    size_t timestamp_end_pos;
-    if (nullptr == timestamp_pattern
-        || false
-                   == timestamp_pattern->parse_timestamp(
-                           m_line,
-                           timestamp,
-                           timestamp_begin_pos,
-                           timestamp_end_pos
-                   ))
-    {
-        timestamp_pattern = TimestampPattern::search_known_ts_patterns(
-                m_line,
-                timestamp,
-                timestamp_begin_pos,
-                timestamp_end_pos
-        );
-    }
-
-    if (nullptr != timestamp_pattern) {
-        // A timestamp was parsed
-        if (m_buffered_msg.is_empty()) {
-            // Fill message with line
-            m_buffered_msg.set(
-                    timestamp_pattern,
-                    timestamp,
-                    m_line,
-                    timestamp_begin_pos,
-                    timestamp_end_pos
-            );
-        } else {
-            // Move buffered message to message
-            message.consume(m_buffered_msg);
-
-            // Save line for next message
-            m_buffered_msg.set(
-                    timestamp_pattern,
-                    timestamp,
-                    m_line,
-                    timestamp_begin_pos,
-                    timestamp_end_pos
-            );
-            message_completed = true;
-        }
-    } else {
-        // No timestamp was parsed
-        if (m_buffered_msg.is_empty()) {
-            // Fill message with line
-            message.set(
-                    timestamp_pattern,
-                    timestamp,
-                    m_line,
-                    timestamp_begin_pos,
-                    timestamp_end_pos
-            );
-            message_completed = true;
-        } else {
-            // Append line to message
-            m_buffered_msg.append_line(m_line);
-        }
-    }
-
+    size_t timestamp_begin_pos{};
+    size_t timestamp_end_pos{};
+    message.set(
+            timestamp_pattern,
+            timestamp,
+            m_line,
+            timestamp_begin_pos,
+            timestamp_end_pos
+    );
     m_line.clear();
-    return message_completed;
+    return true;
 }
 }  // namespace clp
